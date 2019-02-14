06396074ec50c718c63a281dd1e00a3c9932651c
Jingwen Low <jingwenlow@Jingwens-MacBook-Air.local>
2019-02-13 23:54:18 -0800

    frontpage

--- frontend/contrails/urls.py
+++ frontend/contrails/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', HomeView.as_view(), name='home'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
