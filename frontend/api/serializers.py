539f64749cc374c3666725ccebdf1a2ccd705c4f
Joshua Prince <jtprince@ucdavis.edu>
2019-03-11 21:06:03 -0700

    Basic API for instance data

--- frontend/api/serializers.py
+++ frontend/api/serializers.py
@@ -1,2 +1,28 @@
 from rest_framework import serializers
+
 from .models import *
+
+
+class InstanceDataSerializer(serializers.Serializer):
+    region = serializers.CharField(source='location', allow_blank=True, max_length=127)
+    instance_type = serializers.CharField(source='instanceType', allow_blank=True, max_length=127)
+    clockSpeed = serializers.FloatField()
+    memory = serializers.FloatField()
+    retrieved_date = serializers.SerializerMethodField()
+    pricing_type = serializers.SerializerMethodField()
+
+    def get_retrieved_date(self, obj):
+        date = obj.onDemandEffectiveDate or obj.reservedEffectiveDate or obj.spotTimestamp
+        if type(date) == str:
+            return date
+        return date.isoformat()
+
+    def get_pricing_type(self, obj):
+        if obj.onDemandEffectiveDate:
+            return 'on_demand'
+
+        if obj.reservedEffectiveDate:
+            return 'reserved'
+
+        if obj.spotTimestamp:
+            return 'spot'
--- frontend/api/views.py
+++ frontend/api/views.py
@@ -2,6 +2,7 @@ from django.core import serializers
 from django.shortcuts import render
 from rest_framework import status, viewsets, generics
 from rest_framework.decorators import api_view, action
+from rest_framework.request import Request
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import (
@@ -10,10 +11,14 @@ from rest_framework.status import (
     HTTP_200_OK
 )
 
+from infi.clickhouse_orm.database import Database
+
+from loader.loader import InstanceData
 from .models import *
 from .serializers import *
 
-import json
+
+db = Database('contrail', db_url='http://54.153.73.138:8123', readonly=True)
 
 
 class GetInstances(APIView):
@@ -22,19 +27,21 @@ class GetInstances(APIView):
         @Josh
         Return (you can alter if need be)
         {
-            "instance":
-            "operating_system":
+            "retrieved_date":
+            "instance_type":
+            TODO "operating_system":
             "provider":
             "region":
-            "vcpus":
+            TODO "vcpus":
             "memory":
-            "ecu":
-            "pricing_method":
-            "cost":
+            TODO "ecu":
+            "pricing_method": "on_demand", "reserved", "spot"
+            TODO "price":
         }
         '''
-        def post(self, request):
-            data = json.loads(request.body)
+        def post(self, request: Request):
+            c = InstanceData.objects_in(db).only('location', 'instanceType', 'clockSpeed', 'memory',
+                                                 'onDemandEffectiveDate', 'reservedEffectiveDate', 'spotTimestamp').\
+                distinct()#.paginate(page_num=1, page_size=100)
 
-
-            return Response({}, status=HTTP_200_OK)
+            return Response({'instances': [InstanceDataSerializer(obj).data for obj in c]}, status=HTTP_200_OK)
