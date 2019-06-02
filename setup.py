b8a680ad3fd2c3cd8f4cce904bb71066977b474d
Joshua Prince <jtprince@ucdavis.edu>
2019-06-01 21:34:01 -0700

    Version bump for pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme_file:
 
 setuptools.setup(
     name='contrail',
-    version='0.2.7',
+    version='0.3.0',
     description='Public cloud market price tracker for Amazon EC2 and Azure',
     long_description=long_description,
     long_description_content_type='text/markdown',
