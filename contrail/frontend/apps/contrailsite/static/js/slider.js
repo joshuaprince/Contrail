5f4b46e82795481f253210fbccad04c964ac1d68
jingwenlow <jingwlow@gmail.com>
2019-06-04 14:27:59 -0700

    log slider working, but not input boxes

--- contrail/frontend/apps/contrailsite/static/js/slider.js
+++ contrail/frontend/apps/contrailsite/static/js/slider.js
@@ -1,44 +1,30 @@
-var i;
-var minp = 0;
-var maxp = 200;
 
-var pricehr_values = [0];
-var memory_values = [0];
-var vcpu_values = [0];
 
-for(i = minp; i < maxp; i++) {
-    // pricehr
-    var pricehr_minv = Math.log(0.01);
-    var pricehr_maxv = Math.log(30);
-    var scale = (pricehr_maxv-pricehr_minv) / (maxp-minp);
-    var value = Math.exp(pricehr_minv + scale*(i-minp));
-    if(!pricehr_values.includes(value.toFixed(2))){
-        pricehr_values.push(value.toFixed(2))
-    }
-    else maxp++;
 
-    // memory
-    var memory_minv = Math.log(1);
-    var memory_maxv = Math.log(4000);
-    var scale = (memory_maxv-memory_minv) / (maxp-minp);
-    var value = Math.exp(memory_minv + scale*(i-minp));
-    if(!memory_values.includes(value.toFixed(0))){
-        memory_values.push(value.toFixed(0))
-    }
-    else maxp++;
+function logvalues(min, max){
+  var i;
+  var minp = 0;
+  var maxp = 500;
+  var values = [0]
 
-    // vcpu
-    var vcpu_minv = Math.log(1);
-    var vcpu_maxv = Math.log(50);
-    var scale = (vcpu_maxv-vcpu_minv) / (maxp-minp);
-    var value = Math.exp(vcpu_minv + scale*(i-minp));
-    if(!vcpu_values.includes(value.toFixed(0))){
-        vcpu_values.push(value.toFixed(0))
-    }
-    else maxp++;
+  for(i = minp; i < maxp; i++) {
+      var minv = Math.log(min);
+      var maxv = Math.log(max);
+      var scale = (maxv-minv) / (maxp-minp);
+      var value = Math.exp(minv + scale*(i-minp));
+      if(!values.includes(value.toFixed(2))){
+          values.push(value.toFixed(2))
+      }
+  }
+
+  values.push(Math.ceil(value))
+
+  return values;
 }
 
-
+var pricehr_values = logvalues(0.01, 30);
+var memory_values = logvalues(1, 4000);
+var vcpu_values = logvalues(1, 50);
 
 $(".pricehr-range-slider").ionRangeSlider({
      type: "double",
